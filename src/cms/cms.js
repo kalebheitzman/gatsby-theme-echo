b607fb54d3a0362a545e21b9074a1c3d00245dcb
Kaleb Heitzman <kalebheitzman@gmail.com>
2020-09-28 23:46:43 -0400

    updated cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,6 +6,37 @@ import { Control as ColorControl } from './Control'
 import { Timezone } from './Timezone'
 import EventPagePreview from './preview-templates/EventPagePreview'
 
+// import collections 
+import { events, pages } from './collections'
+
+// initialize the cms
+CMS.init({
+	config: {
+
+		// backend configuration
+		backend: {
+			name: "github",
+			repo: process.env.GATSBY_NETLIFY_BACKEND,
+			branch: "develop"
+		},
+
+		// configuration file
+		load_config_file: false,
+
+		// media locations
+		media_folder: "static/assets",
+		public_folder: "/assets",
+
+		// collections
+		collections: [
+			// events
+			events,
+			// Pages
+			pages,
+		]
+  },
+})
+
 // register color widget
 CMS.registerWidget("color", ColorControl);
 CMS.registerWidget("timezone", Timezone);
